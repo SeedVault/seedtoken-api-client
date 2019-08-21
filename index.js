738d224c56c984cf28bcb8ddd6121f7aa81abe7e
Nicolas Wolf <ndwolf@gmail.com>
2019-08-21 16:38:41 -0300

    added library module

---
+++ index.js
@@ -0,0 +1,9 @@
+const SeedTokenAPIClientEthereumETHPersonal = require('./src/SeedTokenAPIClientEthereumETHPersonal.js')
+const SeedTokenAPIClientEthereumETH = null //not available yet
+const SeedTokenAPIClientEthereumERC20 = null //not available yet
+
+module.exports = {
+    SeedTokenAPIClientEthereumETHPersonal,
+    SeedTokenAPIClientEthereumETH,
+    SeedTokenAPIClientEthereumERC20
+}
